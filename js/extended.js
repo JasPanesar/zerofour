cf058ede7796cfa12e88d5755fcccede77f19c5a
Steve Withington <stephenwithington@gmail.com>
2014-02-21 08:19:28 -0600

    add original theme files to assets

--- js/extended.js
+++ js/extended.js
@@ -1,4 +1,9 @@
+/*
+	This file is part of the ZeroFour Mura CMS Theme
 
+	Notes:
+		- this is where custom overrides to both the original theme and Mura CMS scripts is added
+*/
 jQuery(document).ready(function($) {
 
 	// add any custom theme js here
